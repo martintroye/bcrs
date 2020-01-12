3a8da2e284d1a970b9cb60078d00172ff5c198c2
Reva Baumann <rmbaumann@my365.bellevue.edu>
2020-01-12 16:52:27 -0600

    Update user-list.component.ts

--- src/app/pages/admin/user-list/user-list.component.ts
+++ src/app/pages/admin/user-list/user-list.component.ts
@@ -11,66 +11,52 @@
 // Start Program
 
 // Import the Modules
-import {Component, OnInit } from '@anglar/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+// Not sure if this is the correct import - User Dialog
+import {UserDeleteDialogComponent} from '../../dialogs/user-delete-dialog/user-delete-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 import {HttpClient} from '@angular/common/http';
 
-// Details of the component, including route
-@component({
-  selector: 'app-user-details',
-  templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+// Export the component
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
 })
+
 export class UserDetailsComponent implements OnInit {
   user: any;
-  userId: string;
-  form: FormGroup;
-  roles: any;
+  displayedColumns = ['username', 'firstname', 'lastname', 'phoneNumber', 'address', 'email', 'function']
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private fb: FormBuilder, private)
-    this.userId = this.route.snapshot.paramMap.get('userId');
-
-    this.http.get('/api/users/'+ this.userId).subscribe(res => {
+  constructor(private http: HttpClient, private dialog: MatDialog) {
+    this.http.get('/api/users').subscribe(res => {
       this.user = res;
-    }, err => {
+      console.log(this.user);
+    }, err=> {
       console.log(err);
-    }, () => {
-      this.form.controls.firstname.setValue(this.user.firstname);
-      this.form.controls.lastname.setValue(this.user.lastname);
-      this.form.controls.phoneNumber.setValue(this.user.phoneNumber);
-      this.form.controls.address.setValue(this.user.address);
-      this.form.controls.email.setValue(this.user.email);
-      this.form.controls.role.setValue(this.user.role);
     });
-}
+  }
 
-ngOnInit() {
-  this.form = this.fb.group({
-    firstname: [null, Validators.compose([Validators.required])],
-    lastname: [null, Validators.compose([Validators.required])],
-    phoneNumber: [null, Validators.compose([Validators.required])],
-    address: [null, Validators.compose([Validators.required])],
-    email: [null, Validators.compose([Validators.required, Validators.email)],
-    role: [null, Validators.compose([Validators.required])],
-  });
-}
+  ngOnInit() {
 
-saveUser() {
-  this.http.put('/api/users/' + this.userId, {
-    firstname: this.form.controls.fistname.value,
-    lastname: this.form.controls.lastname.value,
-    phoneNumber: this.form.controls.phoneNumber.value,
-    address: this.form.controls.address.value,
-    email: this.form.controls.email.value,
-    role: this.form.controls.role.value
-  }).subscribe(res => {
-    this.router.navigate(['/users']);
-  });
-}
+  }
 
-cancelAnimationFrame() {
-  this.router.navigate(['/users']);
-}
+  delete(userId, username) {
+    const dialogRef = this.dialog.open(UserDeleteDialogComponent, {
+      data: {
+        username
+      },
+      disableClose: true,
+      width: '800px'
+    });
 
-// End Program
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'confirm') {
+        this.http.delete('/api/users/' + userId).subscribe(res => {
+          console.log('User Deleted');
+          this.user = this.user.filter(u => u._id !== userId);
+        });
+      }
+    });
+  }
+}
