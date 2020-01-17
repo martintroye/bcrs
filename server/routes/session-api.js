566a4fa3ed6554c116a55c659c6208a0a4e7c5a3
Reva Baumann <rmbaumann@my365.bellevue.edu>
2020-01-16 18:25:44 -0600

    Update session-api.js

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -176,6 +176,49 @@ router.put('/users/:username/reset-password', (request, response) => {
   }
 });
 
+/**
+ * VerifySecurityQuestions
+ */
+
+ router.post('/verify/users/:username/security-questions', function (req, res, next) {
+   const answerToSecurityQuestion1 = req.body.answerToSecurityQuestion1;
+   console.log(answerToSecurityQuestion1);
+   const answerToSecurityQuestion2 = req.body.answerToSecurityQuestion2;
+   console.log(answerToSecurityQuestion2);
+   const answerToSecurityQuestion3 = req.body.answerToSecurityQuestion3;
+   console.log(answerToSecurityQuestion3);
+
+   User.findOne({'username': req.params.username}, function (err, user) {
+     if (err) {
+       console.log(err);
+       return.next(err);
+     } else {
+       console.log(user);
+
+       let answer1IsValid = answerToSecurityQuestion1 === user.securityQuestions[0].answer;
+       console.log(answer1IsValid);
+
+       let answer1IsValid = answerToSecurityQuestion2 === user.securityQuestions[1].answer;
+       console.log(answer2IsValid);
+
+       let answer1IsValid = answerToSecurityQuestion3 === user.securityQuestions[2].answer;
+       console.log(answer3IsValid);
+
+       if (answer1IsValid && answer2IsValid && answer3IsValid) {
+         res.status(200).send({
+           type: 'success',
+           auth: true
+         })
+       } else {
+         res.status(200).send({
+           type: 'error',
+           auth: false
+         })
+       }
+     }
+   })
+ });
+
 
 /*
 ; Params: Username, answers to 3 security questions
