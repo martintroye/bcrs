79dfc75c79207780969f741d3f3cec366756a786
Reva Baumann <rmbaumann@my365.bellevue.edu>
2020-01-19 19:45:14 -0600

    Create about-us.component.ts

---
+++ src/app/pages/about-us/about-us.component.ts
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title: about-us
+; Author: Reva Baumann
+; Date: 01/06/2020
+; Modified By: Reva Baumann
+; Description: About BCRS
+;===========================================
+*/
+
+// Start Program
+
+// Import the Modules
+import {Copmonent, OnInIt} from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+
+
+// Component details
+@componentFactoryName({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+
+constructor() { }
+
+ngOnInit() {
+
+}
+
+// End the Program
