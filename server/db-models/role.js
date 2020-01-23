660f1b061b002f676d5d80faa6370d16fb22bcfd
Adam <adamdonner@gmail.com>
2020-01-22 20:58:46 -0600

    Update role.js

--- server/db-models/role.js
+++ server/db-models/role.js
@@ -1,7 +1,26 @@
+/*
+============================================
+; Title: role
+; Author: Adam Donner
+; Date: 01/22/2020
+; Modified By: Adam Donner
+; Description: role model
+;===========================================
+*/
+// start program
+
+
 const mongoose = require('mongoose');
 
+
+/**
+ * Declare role schema
+ */
 let roleSchema = mongoose.Schema({
     text: {type: String, unique: true, dropDups: true}
 });
 
+// Export mongoose model
 module.exports = mongoose.model('Role', roleSchema);
+
+// end program
