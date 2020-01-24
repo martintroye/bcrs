9e7489d8efe60bfe09a6f7175d8146807ada9aab
Reva Baumann <rmbaumann@my365.bellevue.edu>
2020-01-23 19:16:40 -0600

    Create invoice-summary-dialog.ts

---
+++ src/app/shared/invoice-summary-dialog/invoice-summary-dialog.ts
@@ -0,0 +1,33 @@
+/*
+============================================
+; Title: invoice-summary-dialog
+; Author: Reva Baumann
+; Date: 01/23/2020
+; Modified By: Reva Baumann
+; Description: Summarize and collect the invoice
+;===========================================
+*/
+
+// Start Program
+
+// Import the Modules
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { componentFactoryName } from '@angular/compiler';
+
+@componentFactoryName({
+  selector: 'app-invoice-summary-dialog',
+  templateUrl: './invoice-summary-dialog.html',
+  styleUrls: ['./invoice-summary-dialog.css']
+})
+
+// Export the class
+export class InvoiceSummaryDialogComponent implements OnInit {
+  invoice: any;
+  constructor(private dialogRef: MatDialogRef<InvoiceSummaryDialogComponent>, @Inject(MAT_DIALOG_DATA) data) {
+    this.invoice = data.invoice;
+  }}
+
+  ngOnInit() {
+
+  }
